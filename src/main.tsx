2f3326aff95dd5aed23769ae7505b8a627e41f6f
trilomohan-spec <trilomohan@gmail.com>
2026-06-17 11:00:59 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { getRouter } from "./router";
+import { LicenseGate } from "./components/LicenseGate";
 import "./styles.css";
 
 const queryClient = new QueryClient();
@@ -10,8 +11,10 @@ const router = getRouter(queryClient);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <LicenseGate>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </LicenseGate>
   </StrictMode>,
 );
